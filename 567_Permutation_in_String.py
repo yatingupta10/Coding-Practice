e5d9142442d6c664516e4584a220f0ce70f23f30
yatingupta10 <yatingupta10@github.com>
2018-09-15 19:48:45 -0700

    Initial solution of 567

--- 567_Permutation_in_String.py
+++ 567_Permutation_in_String.py
@@ -3,24 +3,45 @@ from itertools import permutations
 s = raw_input("input: ")
 s1 = raw_input("input 2 string: ")
 
-s3 = s + s
-s4 = s[::-1] + s[::-1]
+# s3 = s + s
+# s4 = s[::-1] + s[::-1]
 
-print s3
-print s4
+# print s3
+# print s4
 
-perm_list = list()
+# perm_list = list()
 
-for i in range(len(s3)/2):
-	j = i + len(s)
-	perm_list.append(s3[i:j])
-	perm_list.append(s4[i:j])
+# for i in range(len(s3)/2):
+# 	j = i + len(s)
+# 	perm_list.append(s3[i:j])
+# 	perm_list.append(s4[i:j])
 
 # perm_list = [''.join(p) for p in permutations(s)]
 
 # perm_list = list(set(perm_list))
-for i in perm_list:
-	if i in s1:
-		print "T"
+
+char_map = {x:0 for x in s}
+
+for i in range(len(s)):
+	char_map[s[i]] += 1
+# print char_map
+substrings_s1 = [s1[i:i+len(s)] for i in xrange(len(s1) - len(s) + 1)]
+print substrings_s1
+
+substrings_s1_1 = {k:v for k,v in char_map.items()}
+print substrings_s1_1
+for i in substrings_s1:
+	substrings_s1_1 = {k:v for k,v in char_map.items()}
+	for letter in i:
+		if letter in substrings_s1_1:
+			# print "++++++++++++++"
+			substrings_s1_1[letter] -= 1
+	if all(x==0 for x in substrings_s1_1.values()) == True:
+		print "True"
 		break
-print perm_list
+
+# for i in perm_list:
+# 	if i in s1:
+# 		print "T"
+# 		break
+# print perm_list
