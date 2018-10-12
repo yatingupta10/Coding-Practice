8a3840a74062b9a10b0f95d7a386d94c30606c59
yatingupta10 <yatingupta12@gmail.com>
2018-10-12 15:48:55 -0700

    added solution for 905

---
+++ LeetCode/905_Sort_Array_By_Parity.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def sortArrayByParity(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        even = []
+        odd = []
+
+        for i in A:
+            if i%2 == 0:
+                even.append(i)
+            else:
+                odd.append(i)
+
+        return even + odd
