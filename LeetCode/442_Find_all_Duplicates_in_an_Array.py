824ff68245ec4203783ff773efc4c24a94816835
yatingupta10 <yatingupta12@gmail.com>
2018-10-25 13:57:01 -0700

    faster solution added for 442

--- LeetCode/442_Find_all_Duplicates_in_an_Array.py
+++ LeetCode/442_Find_all_Duplicates_in_an_Array.py
@@ -4,15 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        dict_nums = dict()
-        # nums = list(nums)
-        for i in range(len(nums)):
-            if str(nums[i]) not in dict_nums:
-                dict_nums[str(nums[i])] = 1
+        # dict_nums = dict()
+        # # nums = list(nums)
+        # for i in range(len(nums)):
+        #     if str(nums[i]) not in dict_nums:
+        #         dict_nums[str(nums[i])] = 1
+        #     else:
+        #         dict_nums[str(nums[i])] = dict_nums[str(nums[i])] + 1
+        # l = list()
+        # for key, value in dict_nums.items():
+        #     if value == 2:
+        #         l.append(int(key))
+        # return l
+
+        d = {}
+        res = []
+        for i in nums:
+            if i in d:
+                res.append(i)
             else:
-                dict_nums[str(nums[i])] = dict_nums[str(nums[i])] + 1
-        l = list()
-        for key, value in dict_nums.items():
-            if value == 2:
-                l.append(int(key))
-        return l
+                d[i] = 1
+        return res
