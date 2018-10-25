947cdf653bcdd48753af64d162fdd0cc02be48c8
yatingupta10 <yatingupta12@gmail.com>
2018-10-25 13:53:30 -0700

    solution added for 442

---
+++ LeetCode/442_Find_all_Duplicates_in_an_Array.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        dict_nums = dict()
+        # nums = list(nums)
+        for i in range(len(nums)):
+            if str(nums[i]) not in dict_nums:
+                dict_nums[str(nums[i])] = 1
+            else:
+                dict_nums[str(nums[i])] = dict_nums[str(nums[i])] + 1
+        l = list()
+        for key, value in dict_nums.items():
+            if value == 2:
+                l.append(int(key))
+        return l
